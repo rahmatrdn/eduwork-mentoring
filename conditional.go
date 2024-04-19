1b1e1e3c38863bb72db500b4e0b85c3234d64df2
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2024-04-19 15:22:28 +0700

    conditional

--- conditional.go
+++ conditional.go
@@ -5,10 +5,31 @@ import "fmt"
 func main() {
 	age := 23
 
-	if age > 23 {
+	if age > 18 {
 		fmt.Println("Anda Sudah Dewasa")
 	} else {
 		fmt.Println("anda belum dewasa")
+	}
+
+	fmt.Println("============")
+
+	days := 3
+
+	switch days {
+	case 1:
+		fmt.Println("senin")
+	case 2:
+		fmt.Println("selasa")
+	case 3:
+		fmt.Println("rabu")
+	case 4:
+		fmt.Println("kamis")
+	case 5:
+		fmt.Println("jumat")
+	case 6:
+		fmt.Println("sabtu")
+	case 7:
+		fmt.Println("migggu")
 	}
 
 }
