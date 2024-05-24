60519d77b1dcd9470e67dda5582710ab7af0fc2d
faniandrianto <faniandrianto69@gmail.com>
2024-05-24 13:03:58 +0700

    perkenalan

--- bootcamp-go/main.go
+++ bootcamp-go/main.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("hello go")
+	var name = "Fani Andrianto"
+	age := 25
+	siswa := true
+
+	fmt.Println("name : ", name)
+	fmt.Println("age : ", age)
+	fmt.Println("siswa : ", siswa)
 }
